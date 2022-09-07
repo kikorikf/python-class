5e738422c7109045c543f46c8abbd991105bb34f
kikorikf <96519696+kikorikf@users.noreply.github.com>
2022-09-07 13:38:24 +0600

    Add files via upload

---
+++ task2rtr.py
@@ -0,0 +1,8 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/task2rtr.py ==
+a = 12
+b = 10
+h = 1
+The answer is 19.25
