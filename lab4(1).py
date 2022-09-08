f45a46ea92b08ac31a5dfd93dfb384055ba80824
kikorikf <96519696+kikorikf@users.noreply.github.com>
2022-09-08 16:47:47 +0600

    Add files via upload

---
+++ lab4(1).py
@@ -0,0 +1,8 @@
+x = int(input ('The price of 1 kg of sweets '))
+n = int(input ('The number of kg of sweets '))
+for x in [100]:
+    if x == 0: break
+else:
+    print ('The cost of {n} kg of sweets is', x*n)
+             
+             
