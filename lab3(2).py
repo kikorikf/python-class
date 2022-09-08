4190ea7b0cb723a70a263dd20d4918cb0411cd31
kikorikf <96519696+kikorikf@users.noreply.github.com>
2022-09-08 16:26:56 +0600

    Add files via upload

---
+++ lab3(2).py
@@ -0,0 +1,20 @@
+
+shape = int(input('\n1.January\n2.February\n3.March\n4.April\n5.May\n6.June\n7.July\n8.August\n9.September\n10October\n11.November\n12.December\n'))
+if shape == 12:
+     print ('You were born in Winter. White snow fell outside the window')
+
+elif shape < 3:
+        print ('You were born in Winter. White snow fell outside the window')
+elif shape < 6:
+        print ('You were born in Spring. Birds sang beautiful songs')
+elif shape < 9:
+        print ('You were born in Summer. The sun shone brighter than ever')
+else:
+        print ('You were born in Autumn. The harvest was incredible')
+
+
+    
+
+    
+
+        
