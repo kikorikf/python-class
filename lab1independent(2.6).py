e4ab8722367d891a9060c007d0a4d0765b88963d
kikorikf <96519696+kikorikf@users.noreply.github.com>
2022-09-11 01:29:33 +0600

    Add files via upload

---
+++ lab1independent(2.6).py
@@ -0,0 +1,12 @@
+i = 0
+#1
+while (i<100):
+    i += 1
+    if i==50: continue
+    if i==99: continue
+    print(i)
+
+    #2
+for i in range (100):
+    if i == 50: continue
+    if i == 99: continue
